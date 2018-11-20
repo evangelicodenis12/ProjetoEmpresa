58cb630e5115e7981cc21f5934968ea2527e6c64
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2018-11-20 20:15:23 -0200

    Commit Inicial

---
+++ ProjetoEmpresa/src/Funcionario.java
@@ -0,0 +1,54 @@
+
+public class Funcionario {
+	private int codigo;
+	private String nome;
+	private String endereco;
+	private String cidade;
+	private String estado;
+	private double salario;
+	private Cargo cargo;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public Cargo getCargo() {
+		return cargo;
+	}
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	
+}
